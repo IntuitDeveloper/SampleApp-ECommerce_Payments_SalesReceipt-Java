235ccf9be16c3d62fe08221653dfb122b3bea884
Russell E Bolles <russell_bolles@intuit.com>
2014-10-15 10:27:07 -0700

    touching up documentation on OAuthInfoProvider

--- src/main/java/com/intuit/developer/sampleapp/ecommerce/oauth/OAuthInfoProvider.java
+++ src/main/java/com/intuit/developer/sampleapp/ecommerce/oauth/OAuthInfoProvider.java
@@ -1,13 +1,15 @@
 package com.intuit.developer.sampleapp.ecommerce.oauth;
 
 /**
- * TODO: add OAuthInfoProvider overview
+ * An interface that defines operations a given application would need to support in order to 
+ * to access and persist information required during the OAuth 1.0 flow.
  */
 public interface OAuthInfoProvider {
 
     public String getAppToken();
 
     public String getConsumerKey();
+
     public String getConsumerSecret();
 
     /**
@@ -19,10 +21,20 @@ public interface OAuthInfoProvider {
     public void setRequestTokenValuesForCompany(String appCompanyId, String requestToken, String requestTokenSecret);
 
     /**
+     * Lookup the request token secret and appCompanyId by a request token in your app
      *
      * @param requestToken
      * @return
      */
     public CompanyRequestTokenSecret getCompanyRequestTokenSecret(String requestToken);
+
+    /**
+     * Persist the accessToken, accessTokenSecret and realmId in your app for a given company
+     *
+     * @param appCompanyId
+     * @param realmId
+     * @param accessToken
+     * @param accessTokenSecret
+     */
     public void setAccessTokenForCompany(String appCompanyId, String realmId, String accessToken, String accessTokenSecret);
 }
